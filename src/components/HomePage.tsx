25c1bca6d1131ef3568535cd1c59ee72131c912a
Karolina Dbaj <k.dbaj@outlook.com>
2024-11-04 11:38:29 +0100

    app build

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,17 @@
+import './HomePage.css'; 
+
+const HomePage = () => {
+  return (
+    <div
+      className="homepage-container">
+        <h1 className='text'>Plan social media posts
+        in easy and free way!</h1>
+        <div className="content-center">
+        <h1 className='app-name'>Bloom Plan</h1>
+        <h2 className='slogan'>Plan. Bloom. Shine.</h2>
+        </div>
+    </div>
+  );
+};
+
+export default HomePage;
