144c92a0fad9f51aa159ddf6ec2513a52f00a785
Karolina Dbaj <k.dbaj@outlook.com>
2024-11-09 19:33:30 +0100

    imported font to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
+import MainPage from './components/MainPage';
 
 const App: React.FC = () => {
   return (
@@ -10,6 +12,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/mainpage" element={<MainPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Router>
